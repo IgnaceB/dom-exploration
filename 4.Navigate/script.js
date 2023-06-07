70bd28d8845b2cd1454a60d472a4e936949c065f
IgnaceB <ignacebernard@hotmail.com>
2023-06-07 14:39:22 +0200

    exo4

---
+++ 4.Navigate/script.js
@@ -0,0 +1,12 @@
+/*
+*/
+position=document.querySelector("ol")
+listOl=position.children
+position.insertBefore(listOl[listOl.length-1],listOl[0])
+console.log(listOl[0])
+
+sec=document.querySelectorAll("section")
+
+sec[1].insertBefore(sec[2].querySelector("h2"),sec[1].lastElementChild)
+
+sec[2].insertBefore(sec[1].querySelector("h2"), sec[2].firstElementChild)
